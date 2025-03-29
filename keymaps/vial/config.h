004cfaa39ad8ac04e03b71a36d477f0d0967cecf
Aetco <32085506+Aetco@users.noreply.github.com>
2025-03-29 15:48:48 +0800

    Add files via upload

---
+++ keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0xF3, 0xE3, 0x97, 0x8A, 0x1A, 0x76, 0x93, 0x63}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
